77b84f3fca3fd680e83e820914ca8c74fcce5a02
vcnsiqueira <35041704+vcnsiqueira@users.noreply.github.com>
2020-07-29 00:01:01 -0300

    After enable the authentication by email and password, I instantiate the package from Firebase responsible for all the authentication and define all the authentication functions as class methods in the file firebase.js. This way, they are ready to be consumed in the React components

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,4 +1,5 @@
 import app from 'firebase/app';
+import 'firebase/auth';
 
 const config = {
     apiKey: process.env.REACT_APP_API_KEY,
@@ -14,7 +15,35 @@ const config = {
 class Firebase {
     constructor() {
         app.initializeApp(config);
+
+        this.auth = app.auth();
     }
+
+    // *** Auth API ***
+
+    doCreateUserWithEmailAndPassword = (email, password) => { // Sign up method
+        this.auth.createUserWithEmailAndPassword(email, password);
+    };
+
+    doSignInWithEmailAndPassword = (email, password) => { // Sign in method
+        this.auth.signInWithEmailAndPassword(email, password);
+    };
+
+    doSignOut = () => { // Sign out method
+        this.auth.signOut();
+    };
+
+    doPasswordReset = email => {
+        this.auth.sendPasswordResetEmail(email);
+    };
+
+    doPasswordUpdate = password => {
+        this.auth.currentUsers.updatePassword(password);
+    }
+
 }
+
+
+
 
 export default Firebase;
